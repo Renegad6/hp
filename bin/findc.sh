173ec97e12d8bd6b27f4415afef483236819d0c8
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2016-09-23 14:23:46 +0200

    findc

--- bin/findc.sh
+++ bin/findc.sh
@@ -1,1 +1,1 @@
-find . \( -name .svn -o -name work -o -name opkgbuild -o -name 3rdparty \) -prune -o \( -name *.c -o -name *.cpp \)  -exec grep -i $1 \; -print
+find . \( -name .svn -o -name work -o -name opkgbuild -o -name 3rdparty \) -prune -o \( -name *.c -o -name *.cpp \)  -exec grep -i $1 \; -print |xargs grep $1;
