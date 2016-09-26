b819df9665a1e67b4489a1021e880bf8ebb93b7c
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2016-09-26 12:30:13 +0200

    find

--- bin/findc.sh
+++ bin/findc.sh
@@ -2,4 +2,4 @@ if test -z "$1"
 then
     exit -1;
 fi;
-find . \( -name .svn -o -name work -o -name opkgbuild -o -name 3rdparty \) -prune -o \( -name *.c -o -name *.cpp \) -print|xargs grep -i $1;
+find . \( -name .svn -o -name work -o -name opkgbuild -o -name 3rdparty \) -prune -o \( -name *.c -o -name *.cpp \) -print|xargs grep -i $1 2>/dev/null;
