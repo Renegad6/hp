1597bdbc2c7c44b49348d50a940250babfd56844
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2016-11-07 15:47:34 +0100

    mysvncommit

--- bin/mysvncommit.sh
+++ bin/mysvncommit.sh
@@ -9,10 +9,16 @@ getScriptPath () {
 
 source $(getScriptPath)/common.sh;
 
+if test -z "$1";
+then
+    echo "No comments!!";
+    exit -1;
+fi;
+
 if [ "$AE_SANDBOX_PATH" = "$PWD" ];
 then
     echo "trying commit from REAL!!! sandbox!!";
     exit -1;
 fi;
-
+/opt/CollabNet_Subversion/bin/svn commit -m"$1"
 exit 0;
