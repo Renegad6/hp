a2cbcebdf776f5430226f27180f332a1813e86c0
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2016-11-24 12:19:41 +0100

    aefind

--- bin/findc.sh
+++ bin/findc.sh
@@ -2,4 +2,4 @@ if test -z "$1"
 then
     exit -1;
 fi;
-find . \( -name .svn -o -name work -o -name opkgbuild -o -name 3rdparty \) -prune -o \( -name "*.c" -o -name "*.cpp" -o -name "*.h" \) -print|xargs grep -i $1 2>/dev/null;
+aefind "( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.xml" -o -name "*.fsm" -o -name "*.isd" ) -print"|xargs grep -i $1 2>/dev/null;
