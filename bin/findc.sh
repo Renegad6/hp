5022c7f80d1c9d811b9f18a147f73063911feff3
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2017-01-18 10:44:49 +0100

    findc

--- bin/findc.sh
+++ bin/findc.sh
@@ -2,4 +2,4 @@ if test -z "$1"
 then
     exit -1;
 fi;
-aefind "( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.xml" -o -name "*.fsm" -o -name "*.isd" -o -name "*.proto" ) -print"|xargs grep -iwn $1 2>/dev/null;
+aefind "( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.xml" -o -name "*.fsm" -o -name "*.isd" -o -name "*.proto" -o -name "*.py" ) -print"|xargs grep -iwn $1 2>/dev/null;
