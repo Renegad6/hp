0cba9aeeada1b1b300ebbd6577ba5b3a05927a7b
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2017-02-02 10:36:04 +0100

    findc

--- bin/findc.sh
+++ bin/findc.sh
@@ -1,5 +1,6 @@
-if test -z "$1"
+if test $# -eq 0
 then
+    echo "wrong args!";
     exit -1;
 fi;
-aefind "( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.xml" -o -name "*.qml" -o -name "*.fsm" -o -name "*.isd" -o -name "*.proto" -o -name "*.py" ) -print"|xargs grep -iwn $1 2>/dev/null;
+aefind "( -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.xml" -o -name "*.qml" -o -name "*.fsm" -o -name "*.isd" -o -name "*.proto" -o -name "*.py" -o -name "*.tcl" ) -print"|xargs grep $@ 2>/dev/null;
