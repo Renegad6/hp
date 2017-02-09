21740efe1af03f3efb7bfd49c741a5896df016d7
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2017-02-09 14:09:44 +0100

    findasc

--- bin/findasc.sh
+++ bin/findasc.sh
@@ -1,1 +1,1 @@
-aefind -name .svn -prune -o -type f -exec file {} \; |grep -i text|cut -f1 -d':'|xargs grep -i $1 2>/dev/null;
+aefind -type f -exec file {} \; |grep -i text|cut -f1 -d':'|xargs grep -i $1 2>/dev/null;
