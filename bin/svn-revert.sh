eb1a9ae3f1129c879bbbf65bbdb553212790145e
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2017-06-13 09:59:52 +0200

    svn revert

---
+++ bin/svn-revert.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+# $Header:$
+# svn repo compare.
+#...
+
+getScriptPath () {
+  echo ${0%/*}/
+}
+
+source $(getScriptPath)/common.sh;
+
+if [ -z "$AE_SANDBOX_PATH" ]
+then
+    echo "ae_init_sandbox not performed, impossible to detect risks!!!";
+    exit -1;
+fi;
+
+svn status|grep "^[M|A]"| cut -f2- -d" " > /tmp/$$svnrevert;
+svn revert -R .;
+for f in $(cat /tmp/$$svnrevert);
+do
+    echo $f;
+    touch $f;
+done;
+
+exit 0;
