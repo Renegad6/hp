85de3fa87dff0d5b3cdb901113e3c4466f598680
Jordi de Antonio <grupocapgemini-services.jordi.de.antonio@hp.com>
2017-10-16 14:49:52 +0200

    touch

--- bin/svn-touch.sh
+++ bin/svn-touch.sh
@@ -1,3 +1,16 @@
-svn status $AE_SANDBOX_PATH|tee /tmp/$$touch;
-grep "^[M|A]" /tmp/$$touch| cut -f2- -d" "|xargs touch ;
-rm -f /tmp/$$touch;
+if [ -e /tmp/last.touch ];
+then
+# hay un ficheros de modificaciones antiguas.
+    echo "touching old modifications batch";
+    cat /tmp/last.touch;
+    cat /tmp/last.touch|xargs touch;
+    rm /tmp/last.touch;
+else 
+    svn status $AE_SANDBOX_PATH|grep "^[M|A]"| cut -f2- -d" "|tee /tmp/$$touch;
+    if [ -s /tmp/$$touch ];
+    then
+# hay modificaciones, guardarlas
+        echo "new modifications batch!!";
+        mv /tmp/$$touch /tmp/last.touch;
+    fi;
+fi;
